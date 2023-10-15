930c3f334dd42f2bb92d54a20f3eb55dd4752e79
vamsi <vamsikrishna.kuntolla@ongil.ai>
2023-10-15 09:44:35 +0530

    api updates

--- tax_services/helpers.py
+++ tax_services/helpers.py
@@ -3,7 +3,6 @@ from .models import *
 def get_consolidated_data(id):
     return_dict = {"personalDetails":{}, "contactDetails":{}, "incomeDetails":{}, "bankDetails":{}, "dependantDetails":{}, "spouseDetails":{}}
     tax_filing_ins = TaxFiling.objects.get(id=id)
-    return_dict["id"] = tax_filing_ins.id
 
     #// Personal Details
     return_dict["personalDetails"]["userId"] = tax_filing_ins.user.id
@@ -19,137 +18,144 @@ def get_consolidated_data(id):
 
     #// Contact Details
     if tax_filing_ins.user.contact:
-        return_dict["street"]= tax_filing_ins.user.contact.street
-        return_dict["apartment"]=tax_filing_ins.user.contact.apartment_no
-        return_dict["city"]= tax_filing_ins.user.contact.city
-        return_dict["state"]= tax_filing_ins.user.contact.state
-        return_dict["zipCode"]= tax_filing_ins.user.contact.zip_code
-        return_dict["country"]= tax_filing_ins.user.contact.country
-        return_dict["primaryCountryCode"]= tax_filing_ins.user.contact.primary_number_country_code
-        return_dict["primaryPhoneNumber"]= tax_filing_ins.user.contact.primary_number
-        return_dict["secondaryCountryCode"]= tax_filing_ins.user.contact.secondary_number_country_code
-        return_dict["secondaryPhoneNumber"]= tax_filing_ins.user.contact.secondary_number
-        return_dict["contactEmail"]= tax_filing_ins.user.email
+        return_dict["contactDetails"]["street"]= tax_filing_ins.user.contact.street
+        return_dict["contactDetails"]["apartment"]=tax_filing_ins.user.contact.apartment_no
+        return_dict["contactDetails"]["city"]= tax_filing_ins.user.contact.city
+        return_dict["contactDetails"]["state"]= tax_filing_ins.user.contact.state
+        return_dict["contactDetails"]["zipCode"]= tax_filing_ins.user.contact.zip_code
+        return_dict["contactDetails"]["country"]= tax_filing_ins.user.contact.country
+        return_dict["contactDetails"]["primaryCountryCode"]= tax_filing_ins.user.contact.primary_number_country_code
+        return_dict["contactDetails"]["primaryPhoneNumber"]= tax_filing_ins.user.contact.primary_number
+        return_dict["contactDetails"]["secondaryCountryCode"]= tax_filing_ins.user.contact.secondary_number_country_code
+        return_dict["contactDetails"]["secondaryPhoneNumber"]= tax_filing_ins.user.contact.secondary_number
+        return_dict["contactDetails"]["contactEmail"]= tax_filing_ins.user.email
     else:
-        return_dict["street"]= ""
-        return_dict["apartment"]= ""
-        return_dict["city"]= ""
-        return_dict["state"]= ""
-        return_dict["zipCode"]= ""
-        return_dict["country"]= ""
-        return_dict["primaryCountryCode"]= ""
-        return_dict["primaryPhoneNumber"]= ""
-        return_dict["secondaryCountryCode"]= ""
-        return_dict["secondaryPhoneNumber"]= ""
-        return_dict["contactEmail"]= ""
+        return_dict["contactDetails"]["street"]= ""
+        return_dict["contactDetails"]["apartment"]= ""
+        return_dict["contactDetails"]["city"]= ""
+        return_dict["contactDetails"]["state"]= ""
+        return_dict["contactDetails"]["zipCode"]= ""
+        return_dict["contactDetails"]["country"]= ""
+        return_dict["contactDetails"]["primaryCountryCode"]= ""
+        return_dict["contactDetails"]["primaryPhoneNumber"]= ""
+        return_dict["contactDetails"]["secondaryCountryCode"]= ""
+        return_dict["contactDetails"]["secondaryPhoneNumber"]= ""
+        return_dict["contactDetails"]["contactEmail"]= ""
 
     #//Income Details
     if tax_filing_ins.income:
-        return_dict["interestIncome"]= tax_filing_ins.income.interest_income
-        return_dict["dividendIncome"]= tax_filing_ins.income.dividend_income
-        return_dict["soldStocks"]= tax_filing_ins.income.sold_stocks
-        return_dict["soldCrypto"]= tax_filing_ins.income.sold_cryptocurrency
-        return_dict["foreignIncome"]= tax_filing_ins.income.foreign_country_income
-        return_dict["retirementAccounts"]= tax_filing_ins.income.other_benefits
-        return_dict["stateTaxRefund"]= tax_filing_ins.income.last_year_state_tax_refunds
-        return_dict["foreignBankAccount"]= tax_filing_ins.income.foreign_banks_account_balance_exceeding_10000
-        return_dict["foreignAssets"]= tax_filing_ins.income.foreign_assets_value_exceeding_50000
-        return_dict["rentalIncome"]= tax_filing_ins.income.rental_income_in_usa
-        return_dict["income1099"]= tax_filing_ins.income.last_year_1099_misc_nec_income
-        return_dict["incomeDescription"]=tax_filing_ins.income.income_description
-        return_dict["incomeAmount"]= tax_filing_ins.income.income_amount
-        return_dict["taxFiledLastYear"]= tax_filing_ins.income.filed_taxes_last_year
+        return_dict["incomeDetails"]["interestIncome"]= tax_filing_ins.income.interest_income
+        return_dict["incomeDetails"]["dividendIncome"]= tax_filing_ins.income.dividend_income
+        return_dict["incomeDetails"]["soldStocks"]= tax_filing_ins.income.sold_stocks
+        return_dict["incomeDetails"]["soldCrypto"]= tax_filing_ins.income.sold_cryptocurrency
+        return_dict["incomeDetails"]["foreignIncome"]= tax_filing_ins.income.foreign_country_income
+        return_dict["incomeDetails"]["retirementAccounts"]= tax_filing_ins.income.other_benefits
+        return_dict["incomeDetails"]["stateTaxRefund"]= tax_filing_ins.income.last_year_state_tax_refunds
+        return_dict["incomeDetails"]["foreignBankAccount"]= tax_filing_ins.income.foreign_banks_account_balance_exceeding_10000
+        return_dict["incomeDetails"]["foreignAssets"]= tax_filing_ins.income.foreign_assets_value_exceeding_50000
+        return_dict["incomeDetails"]["rentalIncome"]= tax_filing_ins.income.rental_income_in_usa
+        return_dict["incomeDetails"]["income1099"]= tax_filing_ins.income.last_year_1099_misc_nec_income
+        return_dict["incomeDetails"]["incomeDescription"]=tax_filing_ins.income.income_description
+        return_dict["incomeDetails"]["incomeAmount"]= tax_filing_ins.income.income_amount
+        return_dict["incomeDetails"]["taxFiledLastYear"]= tax_filing_ins.income.filed_taxes_last_year
     else:
-        return_dict["interestIncome"]= "no"
-        return_dict["dividendIncome"]= "no"
-        return_dict["soldStocks"]= "no"
-        return_dict["soldCrypto"]= "no"
-        return_dict["foreignIncome"]= "no"
-        return_dict["retirementAccounts"]= "no"
-        return_dict["stateTaxRefund"]= "no"
-        return_dict["foreignBankAccount"]= "no"
-        return_dict["foreignAssets"]= "no"
-        return_dict["rentalIncome"]= "no"
-        return_dict["income1099"]= "no"
-        return_dict["incomeDescription"]= ""
-        return_dict["incomeAmount"]= ""
-        return_dict["taxFiledLastYear"]= "0"
+        return_dict["incomeDetails"]["interestIncome"]= False
+        return_dict["incomeDetails"]["dividendIncome"]= False
+        return_dict["incomeDetails"]["soldStocks"]= False
+        return_dict["incomeDetails"]["soldCrypto"]= False
+        return_dict["incomeDetails"]["foreignIncome"]= False
+        return_dict["incomeDetails"]["retirementAccounts"]= False
+        return_dict["incomeDetails"]["stateTaxRefund"]= False
+        return_dict["incomeDetails"]["foreignBankAccount"]= False
+        return_dict["incomeDetails"]["foreignAssets"]= False
+        return_dict["incomeDetails"]["rentalIncome"]= False
+        return_dict["incomeDetails"]["income1099"]= False
+        return_dict["incomeDetails"]["incomeDescription"]= ""
+        return_dict["incomeDetails"]["incomeAmount"]= ""
+        return_dict["incomeDetails"]["taxFiledLastYear"]= False
 
     #// additional Spouse Details (Initially hidden)
     if tax_filing_ins.user.status == "MARRIED":
-        return_dict["taxPayerStatus"]= tax_filing_ins.user.status
-        return_dict["spouseFirstName"]= tax_filing_ins.user.spouse.first_name
-        return_dict["spouseMiddleInitial"]= tax_filing_ins.user.spouse.middle_name
-        return_dict["spouseLastName"]= tax_filing_ins.user.spouse.last_name
-        return_dict["spouseSsnOrItin"]= tax_filing_ins.user.spouse.ssn
-        return_dict["applyForItin"]= False
-        return_dict["spouseDateOfBirth"]= tax_filing_ins.user.spouse.dob
-        return_dict["spouseGender"]= tax_filing_ins.user.spouse.gender
-        return_dict["spouseOccupation"]= tax_filing_ins.user.spouse.job_title
-        return_dict["spouseResidentialStatus"]= tax_filing_ins.user.spouse.status
-        return_dict["spouseEmail"]= tax_filing_ins.user.spouse.email
+        return_dict["spouseDetails"]["taxPayerStatus"]= tax_filing_ins.user.status
+        return_dict["spouseDetails"]["spouseFirstName"]= tax_filing_ins.user.spouse.first_name
+        return_dict["spouseDetails"]["spouseMiddleInitial"]= tax_filing_ins.user.spouse.middle_name
+        return_dict["spouseDetails"]["spouseLastName"]= tax_filing_ins.user.spouse.last_name
+        return_dict["spouseDetails"]["spouseSsnOrItin"]= tax_filing_ins.user.spouse.ssn
+        return_dict["spouseDetails"]["applyForItin"]= False
+        return_dict["spouseDetails"]["spouseDateOfBirth"]= tax_filing_ins.user.spouse.dob
+        return_dict["spouseDetails"]["spouseGender"]= tax_filing_ins.user.spouse.gender
+        return_dict["spouseDetails"]["spouseOccupation"]= tax_filing_ins.user.spouse.job_title
+        return_dict["spouseDetails"]["spouseResidentialStatus"]= tax_filing_ins.user.spouse.status
+        return_dict["spouseDetails"]["spouseEmail"]= tax_filing_ins.user.spouse.email
     else:
-        return_dict["taxPayerStatus"]= "SINGLE"
-        return_dict["spouseFirstName"]= ""
-        return_dict["spouseMiddleInitial"]= ""
-        return_dict["spouseLastName"]= ""
-        return_dict["spouseSsnOrItin"]= ""
-        return_dict["applyForItin"]= "0" #// Default to "No"
-        return_dict["spouseDateOfBirth"]= ""
-        return_dict["spouseGender"]= ""
-        return_dict["spouseOccupation"]= ""
-        return_dict["spouseResidentialStatus"]= ""
-        return_dict["spouseEmail"]= ""
+        return_dict["spouseDetails"]["taxPayerStatus"]= "SINGLE"
+        return_dict["spouseDetails"]["spouseFirstName"]= ""
+        return_dict["spouseDetails"]["spouseMiddleInitial"]= ""
+        return_dict["spouseDetails"]["spouseLastName"]= ""
+        return_dict["spouseDetails"]["spouseSsnOrItin"]= ""
+        return_dict["spouseDetails"]["applyForItin"]= False #// Default to False
+        return_dict["spouseDetails"]["spouseDateOfBirth"]= ""
+        return_dict["spouseDetails"]["spouseGender"]= ""
+        return_dict["spouseDetails"]["spouseOccupation"]= ""
+        return_dict["spouseDetails"]["spouseResidentialStatus"]= ""
+        return_dict["spouseDetails"]["spouseEmail"]= ""
 
-    if tax_filing_ins.dependants:
+    if tax_filing_ins.dependants and len(list(tax_filing_ins.dependants.all())) > 0:
         dependants = tax_filing_ins.dependants.all()
         for dependant_ins in dependants:
-            return_dict["providedLivingSupport"]= True
-            return_dict["additionalFirstName"]= dependant_ins.first_name
-            return_dict["additionalMiddleInitial"]= dependant_ins.middle_name
-            return_dict["additionalLastName"]= dependant_ins.last_name
-            return_dict["additionalSsnOrItin"]= dependant_ins.ssn
-            return_dict["applyForItin"]= False
-            return_dict[" additionalDateOfBirth"]= dependant_ins.dob
-            return_dict["additionalGender"]= ""
-            return_dict["additionalOccupation"]= ""
-            return_dict["additionalResidentialStatus"]= ""
-            return_dict["additionalEmail"]= ""
+            return_dict["dependantDetails"]["providedLivingSupport"]= True
+            return_dict["dependantDetails"]["additionalFirstName"]= dependant_ins.first_name
+            return_dict["dependantDetails"]["additionalMiddleInitial"]= dependant_ins.middle_name
+            return_dict["dependantDetails"]["additionalLastName"]= dependant_ins.last_name
+            return_dict["dependantDetails"]["additionalSsnOrItin"]= dependant_ins.ssn
+            return_dict["dependantDetails"]["applyForItin"]= False
+            return_dict["dependantDetails"]["additionalDateOfBirth"]= dependant_ins.dob
+            return_dict["dependantDetails"]["additionalGender"]= ""
+            return_dict["dependantDetails"]["additionalOccupation"]= ""
+            return_dict["dependantDetails"]["additionalResidentialStatus"]= ""
+            return_dict["dependantDetails"]["additionalEmail"]= ""
     else:
-        return_dict["providedLivingSupport"]= False
-        return_dict["additionalFirstName"]= ""
-        return_dict["additionalMiddleInitial"]= ""
-        return_dict["additionalLastName"]= ""
-        return_dict["additionalSsnOrItin"]= ""
-        return_dict["applyForItin"]= "0" #// Default to "No"
-        return_dict[" additionalDateOfBirth"]= ""
-        return_dict["additionalGender"]= ""
-        return_dict["additionalOccupation"]= ""
-        return_dict["additionalResidentialStatus"]= ""
-        return_dict["additionalEmail"]= ""
+        return_dict["dependantDetails"]["providedLivingSupport"]= False
+        return_dict["dependantDetails"]["additionalFirstName"]= ""
+        return_dict["dependantDetails"]["additionalMiddleInitial"]= ""
+        return_dict["dependantDetails"]["additionalLastName"]= ""
+        return_dict["dependantDetails"]["additionalSsnOrItin"]= ""
+        return_dict["dependantDetails"]["applyForItin"]= False #// Default to False
+        return_dict["dependantDetails"][" additionalDateOfBirth"]= ""
+        return_dict["dependantDetails"]["additionalGender"]= ""
+        return_dict["dependantDetails"]["additionalOccupation"]= ""
+        return_dict["dependantDetails"]["additionalResidentialStatus"]= ""
+        return_dict["dependantDetails"]["additionalEmail"]= ""
     
     #//Bank Details
     if tax_filing_ins.bank:
-        return_dict["bankingType"]= tax_filing_ins.bank.service_type
-        return_dict[" bankName"]= tax_filing_ins.bank.name
-        return_dict["accountHolderName"]= tax_filing_ins.bank.acc_holder_name
-        return_dict["ownership"]= tax_filing_ins.bank.ownership
-        return_dict["routingNumber"]= tax_filing_ins.routing_number
-        return_dict["confirmRoutingNumber"]= tax_filing_ins.routing_number
-        return_dict["accountNumber"]= tax_filing_ins.bank.account_number
-        return_dict["confirmAccountNumber"]= tax_filing_ins.bank.acc_holder_name
-        return_dict["accountType"]= tax_filing_ins.bank.account_type
-        return_dict["confirmAccountType"]=tax_filing_ins.bank.account_type
+        return_dict["bankDetails"]["bankingType"]= tax_filing_ins.bank.service_type
+        return_dict["bankDetails"][" bankName"]= tax_filing_ins.bank.name
+        return_dict["bankDetails"]["accountHolderName"]= tax_filing_ins.bank.acc_holder_name
+        return_dict["bankDetails"]["ownership"]= tax_filing_ins.bank.ownership
+        return_dict["bankDetails"]["routingNumber"]= tax_filing_ins.routing_number
+        return_dict["bankDetails"]["confirmRoutingNumber"]= tax_filing_ins.routing_number
+        return_dict["bankDetails"]["accountNumber"]= tax_filing_ins.bank.account_number
+        return_dict["bankDetails"]["confirmAccountNumber"]= tax_filing_ins.bank.acc_holder_name
+        return_dict["bankDetails"]["accountType"]= tax_filing_ins.bank.account_type
+        return_dict["bankDetails"]["confirmAccountType"]=tax_filing_ins.bank.account_type
     else:
-        return_dict["bankingType"]= ""
-        return_dict[" bankName"]= ""
-        return_dict["accountHolderName"]= ""
-        return_dict["ownership"]= ""
-        return_dict["routingNumber"]= ""
-        return_dict["confirmRoutingNumber"]= ""
-        return_dict["accountNumber"]= ""
-        return_dict["confirmAccountNumber"]= ""
-        return_dict["accountType"]= ""
-        return_dict["confirmAccountType"]= ""
+        return_dict["bankDetails"]["bankingType"]= ""
+        return_dict["bankDetails"][" bankName"]= ""
+        return_dict["bankDetails"]["accountHolderName"]= ""
+        return_dict["bankDetails"]["ownership"]= ""
+        return_dict["bankDetails"]["routingNumber"]= ""
+        return_dict["bankDetails"]["confirmRoutingNumber"]= ""
+        return_dict["bankDetails"]["accountNumber"]= ""
+        return_dict["bankDetails"]["confirmAccountNumber"]= ""
+        return_dict["bankDetails"]["accountType"]= ""
+        return_dict["bankDetails"]["confirmAccountType"]= ""
+    
+    for key, value in return_dict.items():
+        for sub_key, sub_value in value.items():
+            if (sub_value == None) and (not sub_value == False):
+                return_dict[key][sub_key] = ''
+
+    return_dict["id"] = tax_filing_ins.id
     
     return return_dict
--- tax_services/views.py
+++ tax_services/views.py
@@ -89,24 +89,113 @@ def tax_filing(request):
         context = {"data":None, "status_flag":False, "status":status.HTTP_400_BAD_REQUEST, "message":str(excepted_message)}
         return Response(status=status.HTTP_400_BAD_REQUEST, data= context)
 
-@api_view(['GET', 'POST'])
+@api_view(['POST'])
 @authentication_classes([TokenAuthentication])
 @permission_classes([IsAuthenticated])
 def personal_contact_details(request):
     data = request.data.copy()
     try:
-        if request.method == "GET":
+        if request.method == "POST":
             if 'id' not in data.keys():
                 context = {"data":None, "status_flag":False, "status":status.HTTP_400_BAD_REQUEST, "message":"Tax Filing Id is required"}
                 return Response(status=status.HTTP_400_BAD_REQUEST, data= context)
+            print(data)
+            tax_filing_ins = TaxFiling.objects.get(id=data["id"])
+            
+            del data["id"]
+
+            for key, value in return_dict.items():
+                for sub_key, sub_value in value.items():
+                    if sub_value == "":
+                        return_dict[key][sub_key] = None
+            user_ins = User.objects.get(id=data["personDetails"]["userId"])
+            #// Personal Details
+            if user_ins.first_name !=  data["firstName"]:
+                user_ins.first_name = data["firstName"]
+            if user_ins.middle_name != data["middleName"]:
+                user_ins.middle_name = data["middleName"]
+            if user_ins.last_name !=  data["lastName"]:
+                user_ins.last_name =  data["lastName"]
+            if user_ins.ssn !=  data["ssn"]:
+                user_ins.ssn =  data["ssn"]
+            data["dateOfBirth"]= user_ins.dob
+            data["gender"]= user_ins.gender
+            data["occupation"]= user_ins.job_title
+            data["residentialStatus"]= user_ins.residental_status
+
+            #// Contact Details
+            if user_ins.contact:
+                data["street"]= user_ins.contact.street
+                data["apartment"]=user_ins.contact.apartment_no
+                data["city"]= user_ins.contact.city
+                data["state"]= user_ins.contact.state
+                data["zipCode"]= user_ins.contact.zip_code
+                data["country"]= user_ins.contact.country
+                data["primaryCountryCode"]= user_ins.contact.primary_number_country_code
+                data["primaryPhoneNumber"]= user_ins.contact.primary_number
+                data["secondaryCountryCode"]= user_ins.contact.secondary_number_country_code
+                data["secondaryPhoneNumber"]= user_ins.contact.secondary_number
+                data["contactEmail"]= user_ins.email
+
+    
+            #// additional Spouse Details (Initially hidden)
+            if user_ins.status == "MARRIED":
+                data["taxPayerStatus"]= user_ins.status
+                data["spouseFirstName"]= user_ins.spouse.first_name
+                data["spouseMiddleInitial"]= user_ins.spouse.middle_name
+                data["spouseLastName"]= user_ins.spouse.last_name
+                data["spouseSsnOrItin"]= user_ins.spouse.ssn
+                data["applyForItin"]= False
+                data["spouseDateOfBirth"]= user_ins.spouse.dob
+                data["spouseGender"]= user_ins.spouse.gender
+                data["spouseOccupation"]= user_ins.spouse.job_title
+                data["spouseResidentialStatus"]= user_ins.spouse.status
+                data["spouseEmail"]= user_ins.spouse.email
+            else:
+                data["taxPayerStatus"]= "SINGLE"
+                data["spouseFirstName"]= ""
+                data["spouseMiddleInitial"]= ""
+                data["spouseLastName"]= ""
+                data["spouseSsnOrItin"]= ""
+                data["applyForItin"]= False #// Default to False
+                data["spouseDateOfBirth"]= ""
+                data["spouseGender"]= ""
+                data["spouseOccupation"]= ""
+                data["spouseResidentialStatus"]= ""
+                data["spouseEmail"]= ""
+
+            if tax_filing_ins.dependants and len(list(tax_filing_ins.dependants.all())) > 0:
+                dependants = tax_filing_ins.dependants.all()
+                for dependant_ins in dependants:
+                    data["providedLivingSupport"]= True
+                    data["additionalFirstName"]= dependant_ins.first_name
+                    data["additionalMiddleInitial"]= dependant_ins.middle_name
+                    data["additionalLastName"]= dependant_ins.last_name
+                    data["additionalSsnOrItin"]= dependant_ins.ssn
+                    data["applyForItin"]= False
+                    data["additionalDateOfBirth"]= dependant_ins.dob
+                    data["additionalGender"]= ""
+                    data["additionalOccupation"]= ""
+                    data["additionalResidentialStatus"]= ""
+                    data["additionalEmail"]= ""
+            else:
+                data["providedLivingSupport"]= False
+                data["additionalFirstName"]= ""
+                data["additionalMiddleInitial"]= ""
+                data["additionalLastName"]= ""
+                data["additionalSsnOrItin"]= ""
+                data["applyForItin"]= False #// Default to False
+                data[" additionalDateOfBirth"]= ""
+                data["additionalGender"]= ""
+                data["additionalOccupation"]= ""
+                data["additionalResidentialStatus"]= ""
+                data["additionalEmail"]= ""
+            
 
             context = {"data":None, "status_flag":True, "status":status.HTTP_200_OK, "message":None}
             return Response(status=status.HTTP_200_OK, data= context)
-        elif request.method == "POST":
-            context = {"data":None, "status_flag":True, "status":status.HTTP_200_OK, "message":None}
-            return Response(status=status.HTTP_200_OK, data= context)
         else:
-            context = {"data":None, "status_flag":True, "status":status.HTTP_200_OK, "message": "Only GET & POST Method Available"}
+            context = {"data":None, "status_flag":True, "status":status.HTTP_200_OK, "message": "Only POST Method Available"}
             return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED, data= context)
     except Exception as excepted_message:
         print(str(excepted_message))
@@ -184,6 +273,38 @@ def income_details(request):
             context = {"data":None, "status_flag":True, "status":status.HTTP_200_OK, "message":None}
             return Response(status=status.HTTP_200_OK, data= context)
         elif request.method == "POST":
+            #//Income Details
+            if tax_filing_ins.income:
+                data["interestIncome"]= tax_filing_ins.income.interest_income
+                data["dividendIncome"]= tax_filing_ins.income.dividend_income
+                data["soldStocks"]= tax_filing_ins.income.sold_stocks
+                data["soldCrypto"]= tax_filing_ins.income.sold_cryptocurrency
+                data["foreignIncome"]= tax_filing_ins.income.foreign_country_income
+                data["retirementAccounts"]= tax_filing_ins.income.other_benefits
+                data["stateTaxRefund"]= tax_filing_ins.income.last_year_state_tax_refunds
+                data["foreignBankAccount"]= tax_filing_ins.income.foreign_banks_account_balance_exceeding_10000
+                data["foreignAssets"]= tax_filing_ins.income.foreign_assets_value_exceeding_50000
+                data["rentalIncome"]= tax_filing_ins.income.rental_income_in_usa
+                data["income1099"]= tax_filing_ins.income.last_year_1099_misc_nec_income
+                data["incomeDescription"]=tax_filing_ins.income.income_description
+                data["incomeAmount"]= tax_filing_ins.income.income_amount
+                data["taxFiledLastYear"]= tax_filing_ins.income.filed_taxes_last_year
+            else:
+                data["interestIncome"]= False
+                data["dividendIncome"]= False
+                data["soldStocks"]= False
+                data["soldCrypto"]= False
+                data["foreignIncome"]= False
+                data["retirementAccounts"]= False
+                data["stateTaxRefund"]= False
+                data["foreignBankAccount"]= False
+                data["foreignAssets"]= False
+                data["rentalIncome"]= False
+                data["income1099"]= False
+                data["incomeDescription"]= ""
+                data["incomeAmount"]= ""
+                data["taxFiledLastYear"]= False
+
             context = {"data":None, "status_flag":True, "status":status.HTTP_200_OK, "message":None}
             return Response(status=status.HTTP_200_OK, data= context)
         else:
