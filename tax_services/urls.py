d8c38b80cfd40c2dc5a46ee26798c8078cbd4dd4
vamsi <vamsikrishna.kuntolla@ongil.ai>
2023-10-22 09:48:07 +0530

    api update

--- tax_services/urls.py
+++ tax_services/urls.py
@@ -3,7 +3,7 @@ from .views import *
 
 urlpatterns = [
     path('', index),
-    path('app/profile/', application),
+    path('app/home/', application),
     path('api/v1/tax_filing/', tax_filing),
     path('api/v1/personal_contact_details/', personal_contact_details),
     path('api/v1/spouse_details/', spouse_details),
